c6b5275597efc8b0d4af6c34d56960ce8937ee7a
Dao Cu Nam <daocunam225@gmail.com>
2025-02-18 13:38:01 +0700

    2nd commit: config JWT tokens with Spring Security

--- book-social-network/src/main/java/com/cunam/book_network/BookNetworkApiApplication.java
+++ book-social-network/src/main/java/com/cunam/book_network/BookNetworkApiApplication.java
@@ -2,8 +2,10 @@ package com.cunam.book_network;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkApiApplication.class, args);
